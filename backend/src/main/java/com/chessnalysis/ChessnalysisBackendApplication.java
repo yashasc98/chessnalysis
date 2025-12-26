ed2f12ab9e9d9b60738a5659a5c335648bbbfd69
Yashas Chandra <yashas998@gmail.com>
2025-12-26 13:02:41 +0530

    Baseline for frontend and backend.

---
+++ backend/src/main/java/com/chessnalysis/ChessnalysisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chessnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessnalysisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessnalysisBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chessnalysis/ChessnalysisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessnalysisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
